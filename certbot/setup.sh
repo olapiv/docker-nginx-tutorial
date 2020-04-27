6f8af1a49f1931d692f83fc2c9bf01a82f12c228
Vincent Lohse <vincent.lohse@live.de>
2020-04-28 01:58:52 +0200

    Added sockets to connect Nginx and Certbot. Added netcat-openbsd for Docker API call in Certbot. Changed certbot image.  Let production also create dummy  keys in Certbot. Forcing Nginx to restart from Certbot.

--- certbot/setup.sh
+++ certbot/setup.sh
@@ -29,15 +29,24 @@ if [ ! -z "$(ls -A $domain_path)" ]; then
     trap exit TERM; while :; do certbot renew; sleep 12h & wait $!; done;
 fi
 
-if [ $DEV == true ]; then
-    echo "### Creating dummy certificate for $domains ..."
-    openssl req -x509 -nodes -newkey rsa:1024 -days 1\
-        -keyout "$domain_path/privkey.pem" \
-        -out "$domain_path/fullchain.pem" \
-        -subj "/CN=localhost"
+echo "### Creating dummy certificate for $domains ..."
+openssl req -x509 -nodes -newkey rsa:1024 -days 1\
+    -keyout "$domain_path/privkey.pem" \
+    -out "$domain_path/fullchain.pem" \
+    -subj "/CN=localhost"
 
+if [ $DEV == true ]; then
     trap exit TERM; while :; do certbot renew; sleep 12h & wait $!; done;
 fi
+
+echo "Wait for Nginx to start properly first time";
+sleep 15s;
+
+# Nginx will cache it's certificates
+echo "### Deleting dummy certificate for $domains ..."
+rm -Rf /etc/letsencrypt/live/$domains && \
+    rm -Rf /etc/letsencrypt/archive/$domains && \
+    rm -Rf /etc/letsencrypt/renewal/$domains.conf
 
 echo "### Requesting Let's Encrypt certificate for $domains ..."
 domain_args=""  # --> "-d your_domain.com -d www.your_domain.com"
@@ -51,11 +60,20 @@ case "$SSL_EMAIL" in
   *) email_arg="--email $SSL_EMAIL" ;;
 esac
 
-certbot certonly --webroot -w $www_path \
+certbot certonly \
+    --webroot \
+    --webroot-path $www_path \
+    --non-interactive \
+    $staging_arg \
     $email_arg \
     $domain_args \
     --rsa-key-size $rsa_key_size \
     --agree-tos \
     --force-renewal
+
+sleep 30s;  # Wait for Certbot to be finished
+
+echo "Force restarting Nginx from Certbot"
+echo -e "POST /containers/$NGINX_CONTAINER/restart HTTP/1.0\r\n" | nc -U /tmp/docker.sock
 
 trap exit TERM; while :; do certbot renew; sleep 12h & wait $!; done;
